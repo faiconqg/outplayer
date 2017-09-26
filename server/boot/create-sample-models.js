187c504f88918d6fd49a919c9be820c87bb1bbb2
Thiago Lins <thiagolins@faicon.com>
2017-09-25 23:43:45 -0300

    Primeiro deploy

--- server/boot/create-sample-models.js
+++ server/boot/create-sample-models.js
@@ -1,15 +1,15 @@
-module.exports = function(app) {
-    app.dataSources.mysqlLocal.automigrate('Merchant', function(err) {
-      if (err) throw err;
+// module.exports = function(app) {
+//     app.dataSources.mysqlLocal.automigrate('Merchant', function(err) {
+//       if (err) throw err;
   
-      app.models.Merchant.create([{
-        name: 'Faiconl Cafe'
-      }, {
-        name: 'Faicon Farm'
-      }, ], function(err, merchants) {
-        if (err) throw err;
+//       app.models.Merchant.create([{
+//         name: 'Faiconl Cafe'
+//       }, {
+//         name: 'Faicon Farm'
+//       }, ], function(err, merchants) {
+//         if (err) throw err;
   
-        console.log('Models created: \n', merchants);
-      });
-    });
-  };
+//         console.log('Models created: \n', merchants);
+//       });
+//     });
+//   };
